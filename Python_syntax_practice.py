b2f05a585c313b7b14d0d742296c8abf359bc3c2
shakshi gandhi <shakshigandhi.a@gmail.com>
2026-01-10 15:05:41 -0800

    Simple syntax understanding

---
+++ Python_syntax_practice.py
@@ -0,0 +1,19 @@
+"""print("Hello world!")"""
+
+'''
+import sys
+print(sys.version)
+'''
+
+'''
+print("Hi!", end = "")
+print("I am Shakshi!")
+'''
+
+'''
+print("Hi! Answer to all questions of the world is: ", 42)
+'''
+
+a = 5
+print(type(a))
+print(type(str(a)))

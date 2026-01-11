2200e43b333ff3861c680468309daa1a18338d76
shakshi gandhi <shakshigandhi.a@gmail.com>
2026-01-10 17:31:59 -0800

    Edits to code

--- Python_syntax_practice.py
+++ Python_syntax_practice.py
@@ -14,6 +14,55 @@ print("I am Shakshi!")
 print("Hi! Answer to all questions of the world is: ", 42)
 '''
 
+'''
 a = 5
 print(type(a))
 print(type(str(a)))
+'''
+
+'''a = b = c = "Mango"
+print(a, b)
+
+fruits = ["Mango", "Banana"]
+x, y = fruits
+print(x, y)'''
+
+'''lang = "SQL"
+z=4
+print(f"Hi my fav language is: {lang}. \n And I have {z} years of exp")'''
+
+'''list [], tuple (), set {}, dict {}'''
+
+'''slicing, len(a), casting'''
+
+'''z=4
+a = "Shakshi"
+#Slicing
+print(a[1:3])
+#length
+print(len(a))
+#operations
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace('a','z'))
+print(len(a.split("k")))
+print(f"Hi! {z:.4f}")
+print(a.find("s"))
+print((a+" gandhi").title())'''
+
+'''
+#Python operators
+a = 5
+b = 6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)'''
+
+'''is, is not, in, not in'''
+
+#Lists
